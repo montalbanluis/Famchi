0ecbaaaed5e11ade0f311c55c0c6f3595f041e59
lamvhack <lamvhaccc@gmail.com>
2021-04-30 15:50:19 -0400

    Primero Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Aplicacion web de gestion de informacion FAMCHI
---
+++ mainapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+
+
+    return HttpResponse("<h1>hola</h1>")
+
+
+    
+
